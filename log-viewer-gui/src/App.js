d200b862ae5a79020c1d32e51e782ac6acab8782
Peter Mocnik <peter.mocnik@teletech.si>
2022-07-05 16:58:29 +0200

    Add React.js GUI with create-react-app.

---
+++ log-viewer-gui/src/App.js
@@ -0,0 +1,13 @@
+import SignIn from "./components/SignIn";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SignIn/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ log-viewer-gui/src/components/SignIn.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const login = async (username, password) => {
+    const params = new URLSearchParams();
+    params.append('username', username);
+    params.append('password', password);
+
+    const response = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        body: params
+    })
+
+
+}
+
+//Component from Material UI
+
+const theme = createTheme();
+
+export default function SignIn() {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        let username = data.get('username');
+        let password = data.get('password');
+        login(username, password);
+        // const params = new URLSearchParams();
+        // params.append('username', data.get('username'));
+        // params.append('password', data.get('password'));
+
+        // const response = await fetch("http://localhost:3000/api/login", {
+        //     method: "POST",
+        //     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        //     body: params
+        // })
+
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
