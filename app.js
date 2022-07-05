d714bf0e2e5c8f922d881d29e31dde06834952de
Peter Mocnik <peter.mocnik@teletech.si>
2022-07-05 16:57:48 +0200

    Add cors for DEV.

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const api = require('./Api/api.js');
 const port = process.env.port || 3000;
+const path = require('path');
+var cors = require('cors');
 
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use("/", express.static(path.join(__dirname, 'log-viewer-gui/build')));
 
-app.use('/api', api)
+app.use('/api', api);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
