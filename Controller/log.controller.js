550e6618c2bba81bdf074b0152afe8dd90f5b112
Peter Mocnik <peter.mocnik@teletech.si>
2022-07-05 16:57:32 +0200

    Add severity level list to config and check in log.controller.js.

--- Controller/log.controller.js
+++ Controller/log.controller.js
@@ -1,18 +1,24 @@
+const { severity_levels } = require("../config");
 const { addLog } = require("../Db/log");
 const { getProject } = require("../Db/project");
 
 const addLogController = async (req, res) => {
     let log = req.body;
-
-    log.project_id = await (await getProject(log.project_short_name))._id;
-
     try {
+        if (!severity_levels.includes(log.severity_level)) throw new Error("Severity level not ok. Must be one of: " + severity_levels.toString());
+
+        const project = await getProject(log.project_short_name);
+
+        if (project == null) throw new Error("Project does not exists!");
+
+        log.project_id = project._id;
+
         await addLog(log);
         res.status(200).send();
     } catch (err) {
-        res.status(202).json(err);
+        console.log(err);
+        res.status(202).json(err.message);
     }
-
 }
 
 module.exports = { addLogController };
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
     jwtSecret: "QrDCTldbDAjJ0Q3IU7DxAJCpTErCdvqECf6xeuEpgJyzWvJAIxgjiDV7mJxorWJ",
-    mongooseConnect: "mongodb://192.168.1.11:27017/log-viewer"
+    // mongooseConnect: "mongodb://192.168.1.11:27017/log-viewer"
+    mongooseConnect: "mongodb://localhost:27017/log-viewer",
+    severity_levels: ["emerg", "alert", "crit", "err", "warning", "notice", "info", "debug"]
 }
